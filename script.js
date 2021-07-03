abd3cae823b13db9670aa100e35f285a536931d2
변찬우 <chan00106@naver.com>
2021-07-03 19:54:43 +0900

    test

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const canvas = document.querySelector(".canvas");
 const cvs = canvas.getContext("2d");
+const colors = document.querySelectorAll(".li");
+const clear = document.querySelector(".clear");
 
+vcolors = {
+    "red": "#d63031",
+    "blue": "#0984e3",
+    "white": "#ffffff",
+    "yellow": "#f9ca24",
+    "black": "#2d3436",
+    "green": "#2ecc71"
+}
+
+canvas.width = 1200;
+canvas.height = 600;
 
 canvas.addEventListener('mousemove', e => {
-    if (e.buttons == 1){
-        cvs.beginPath();
-        cvs.moveTo(e.offsetX, e.offsetY);
-        cvs.lineTo(e.offsetX, e.offsetY);
+    let x = e.offsetX;
+    let y = e.offsetY;
+    if (e.buttons === 1){
+        cvs.lineTo(x, y);
         cvs.stroke();
-        console.log(e   );
+    }
+    else {
+        cvs.beginPath();
+        cvs.moveTo(x, y);
     }
 });
 
+function chageColor(e){
+    const color = e.target.textContent;
+    cvs.strokeStyle = vcolors[color];
+}
 
+Array.from(colors).forEach(color => color.addEventListener("click", chageColor));
+// ctx.lineWidth += 1.5; 두께
 
+clear.addEventListener('click', () => cvs.clearRect(0, 0, canvas.width, canvas.height));
+
+cvs.strokeStyle = vcolors["black"];
